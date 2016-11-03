860ad53736ff0de5e926b258929c66982ae4df11
FancyJesse <jesus_andrade45@yahoo.com>
2016-11-03 11:13:58 -0700

    Completed Challenge #21 and edits to challenge.txt

--- src/challenge21/easy/Main.java
+++ src/challenge21/easy/Main.java
@@ -1,13 +1,60 @@
 package challenge21.easy;
 
-/*
- * INCOMPLETE
- *
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
+	private static int getInput(String prompt) {
+		while (true) {
+			try {
+				System.out.print(prompt + "\n> ");
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				return Integer.parseInt(br.readLine());
+			} catch (Exception e) {
+				System.out.println("Something Went Wrong. Try Again.");
+			}
+		}
+	}
+
+	// finds next number that contains all digits in num
+	private static void findNextNumber(int num) {
+		char[] cDigits = String.valueOf(num).toCharArray();
+
+		// set limit to 9x(length)
+		int limit = Integer.parseInt(new String(new char[cDigits.length]).replace("\0", "9"));
+
+		int nextNum = -1;
+		for (int i = num + 1; i <= limit; i++) {
+
+			String sNumber = String.valueOf(i);
+			for (char c : cDigits) {
+				String sChar = String.valueOf(c);
+
+				if (sNumber.contains(sChar))
+					sNumber = sNumber.replaceFirst(sChar, "F");
+				else
+					break;
+			}
+
+			if (sNumber.matches("[F]+")) {
+				nextNum = i;
+				break;
+			}
+		}
+
+		if (nextNum != -1)
+			System.out.println("\nNext Number: " + nextNum);
+		else
+			System.out.println("\nUnable To Find Next Number For: " + num);
+
+	}
+
 	public static void main(String[] args) {
+		int number;
+
+		number = getInput("Enter a Number:");
+		findNextNumber(number);
 	}
 
 }
--- src/challenge21/hard/Main.java
+++ src/challenge21/hard/Main.java
@@ -1,13 +1,81 @@
 package challenge21.hard;
 
-/*
- * INCOMPLETE
- *
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 
+	private static Set<String> inputSmartPersonSet(String prompt) {
+		Set<String> set = new TreeSet<>();
+
+		System.out.println(prompt);
+		while (true) {
+			try {
+				System.out.print("\n> ");
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				String input = br.readLine().trim();
+				if (input.matches("\\w+ : \\w+")) {
+					set.add(input);
+					System.out.println("Added: " + input);
+				} else if (input.equals("q")) {
+					System.out.println("Returning: " + set.toString() + "\n");
+					return set;
+				} else {
+					System.out.println("Incorrect Formatting. Try Again.");
+				}
+			} catch (Exception e) {
+				System.out.println("Something Went Wrong. Try Again.");
+			}
+		}
+	}
+
+	private static void organizeBySmartness(Set<String> personSetStr) {
+		List<String> smartPersonList = new ArrayList<>();
+
+		for (String personSet : personSetStr) {
+			String[] person = personSet.split(" : ");
+
+			if (person[0].equals(person[1])) {
+				System.out.println("Ignoring Duplicate Case: " + personSet);
+				continue;
+			}
+
+			// neither are contained, simply add
+			if (!(smartPersonList.contains(person[0]) || smartPersonList.contains(person[1]))) {
+				smartPersonList.add(person[0]);
+				smartPersonList.add(person[1]);
+			}
+			// contains at least 1 person
+			else {
+				// contains dumber person, add smarter person in front
+				if (smartPersonList.contains(person[1])) {
+					smartPersonList.add(smartPersonList.indexOf(person[1]), person[0]);
+				}
+				// contains smart person, add dumber person behind
+				else {
+					smartPersonList.add(smartPersonList.indexOf(person[0]) + 1, person[1]);
+				}
+			}
+
+		}
+
+		System.out.println("\nSMART LIST");
+		for (int i = 0; i < smartPersonList.size(); i++)
+			System.out.println(smartPersonList.get(i));
+
+	}
+
 	public static void main(String[] args) {
+
+		Set<String> personsSetStr = inputSmartPersonSet(
+				"Enter Smart-Person Set -- [Enter 'q' to End]\nFORMAT: [Smarter-Person] : [Dumber-Person]");
+
+		organizeBySmartness(personsSetStr);
+
 	}
 
 }
